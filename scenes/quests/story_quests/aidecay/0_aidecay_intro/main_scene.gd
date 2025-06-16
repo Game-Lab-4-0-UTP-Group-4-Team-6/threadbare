7110bb7a0a0a27a0cc49ffd24479431d4d87b568
Joel <joel.mamani.100401@gmail.com>
2025-06-16 15:09:03 -0500

    minigame-one-tilemap

--- scenes/quests/story_quests/aidecay/0_aidecay_intro/main_scene.gd
+++ scenes/quests/story_quests/aidecay/0_aidecay_intro/main_scene.gd
@@ -7,7 +7,8 @@ extends Node2D
 var lines = [
 
 	"¿Qué sucedió?",
-	"¿Dónde estoy?"
+	"¿Dónde estoy?",
+	" "
 ]
 
 var current_line = 0
@@ -21,13 +22,8 @@ func _ready():
 	show_next_line()
 	await get_tree().create_timer(2.5).timeout
 	show_next_line()
-	# A los 10 segundos, empieza el temporizador de la alarma (o ya autostart)
-	alarm_timer.start()
 
 func show_next_line():
 	if current_line < lines.size():
 		bubble.text = lines[current_line]
 		current_line += 1
-
-func _on_AlarmTimer_timeout():
-	alarm_player.play()
